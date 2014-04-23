9874c40a0c6a0584622c07ceb7de543c96d53661
rpedde <ron@pedde.com>
2014-04-23 15:21:33 -0500

    first pass

---
+++ mkbundle.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+BASE_DIR=$(dirname $(readlink -f $0))
+
+function on_exit() {
+    [ -e "${TARBALL}" ] && rm "${TARBALL}"
+}
+
+VERSION=$(printf "%05d" ${BUILD_NUMBER:-$(date +%Y%m%d%H%M)})
+PACKAGE=${PACKAGE:-zvm-zpipes}
+TARBALL=${BASE_DIR}/${PACKAGE}-${VERSION}.tar.gz
+trap on_exit exit
+
+mkcbundle -p ${PACKAGE} -v ${VERSION}
+mkdir -p /var/www/cbundles/${PACKAGE}
+cp ${TARBALL} /var/www/cbundles/${PACKAGE}
+
+
+
+
